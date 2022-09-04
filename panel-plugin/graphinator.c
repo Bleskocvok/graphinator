25f4bdfd85505e8e1647cd9bd69fae483d99bced
František Bráblík <frantisek.brablik@gmail.com>
2022-09-04 10:11:41 +0200

    Add linear graph draw mode

--- panel-plugin/graphinator.c
+++ panel-plugin/graphinator.c
@@ -24,6 +24,7 @@ static double collect_mem_data( void* ptr );
 
 
 static void draw_led( GtkWidget* widget, cairo_t* cr, void* ptr );
+static void draw_lin( GtkWidget* widget, cairo_t* cr, void* ptr );
 
 
 static section_t section =
@@ -38,7 +39,7 @@ static section_t section =
                         .rgb_on  = MK_RGB( 255, 128, 128 ),
                         .rgb_off = MK_RGB( 102, 102, 102 ),  },
 
-    .draw_func = draw_led,
+    .draw_func = draw_lin,
     .interval = 750,
     .max_value = 100,
     .use_max_value = true,
@@ -209,6 +210,54 @@ static void draw_led( GtkWidget* widget, cairo_t* cr, void* ptr )
             cairo_fill( cr );
         }
     }
+}
+
+
+static void draw_lin( GtkWidget* widget, cairo_t* cr, void* ptr )
+{
+    section_t* sec = ptr;
+
+    const graph_t g = sec->graph;
+
+    int cols = g.w / ( g.blk_w + g.pad_x );
+
+    int cap = data_capacity( &sec->data );
+    cols = M_MIN( cap, cols );
+
+    int count = data_count( &sec->data );
+
+    double blk = g.h / (double) sec->max_value;
+
+    gtk_render_background( gtk_widget_get_style_context( widget ),
+                           cr, 0, 0, g.w, g.h);
+
+    cairo_set_source_rgb( cr, g.rgb_on[ 0 ], g.rgb_on[ 1 ], g.rgb_on[ 2 ] );
+
+    if ( count <= 0 )
+        return;
+
+    double prev = count - cols < 0 ? 0
+                                   : blk * data_at( &sec->data, count - cols );
+    cairo_move_to( cr, g.w, g.h );
+    cairo_line_to( cr,   0, g.h );
+    cairo_line_to( cr,   0, g.h - prev );
+
+    for ( int x = 1; x < cols; ++x )
+    {
+        int i = x + count - cols;
+        double y = x < cols - count ? 0
+                                    : blk * data_at( &sec->data, i );
+        cairo_line_to( cr, ( g.blk_w + g.pad_x ) * x, g.h - y );
+        prev = y;
+    }
+
+    cairo_line_to( cr, g.w, g.h - prev );
+    cairo_close_path( cr );
+
+    cairo_set_line_width( cr, 0.5 );
+    cairo_stroke_preserve( cr );
+
+    cairo_fill( cr );
 }
 
 
