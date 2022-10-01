259f8570e0e4a8c0939844aa73bcafa1a427c4c4
František Bráblík <frantisek.brablik@gmail.com>
2022-10-01 16:29:42 +0200

    Remove default sections

--- panel-plugin/graphinator.c
+++ panel-plugin/graphinator.c
@@ -13,44 +13,6 @@
 #include <stdio.h>          // snprintf
 
 
-// static section_t section =
-// {
-//     .collector = (collector_t){ .collect = collect_cpu_data,
-//                                 .init = init_cpu_data,
-//                                 .free = free_cpu_data,       },
-
-//     .graph = (graph_t){ .blk_w =  2, .blk_h =  1,
-//                         .pad_x =  1, .pad_y =  1,
-//                         .h     = 28, .w     = 33,
-//                         .rgb_on  = MK_RGB( 255, 128, 128 ),
-//                         .rgb_off = MK_RGB( 102, 102, 102 ),
-//                         .max_value = 100,                    },
-
-//     .draw_func = draw_lin,
-//     .interval = 750,
-//     .label_fmt = " cpu\n%3.0f%% ",
-// };
-
-
-// static section_t sec_mem =
-// {
-//     .collector = (collector_t){ .collect = collect_mem_data,
-//                                 .init = init_mem_data,
-//                                 .free = free_mem_data,       },
-
-//     .graph = (graph_t){ .blk_w =  2, .blk_h =  1,
-//                         .pad_x =  1, .pad_y =  1,
-//                         .h     = 28, .w     = 33,
-//                         .rgb_on  = MK_RGB( 221, 187,  51 ),
-//                         .rgb_off = MK_RGB( 102, 102, 102 ),
-//                         .max_value = 100,                    },
-
-//     .draw_func = draw_lin,
-//     .interval = 750,
-//     .label_fmt = " mem\n%3.0f%% ",
-// };
-
-
 void entry_refresh( panel_entry_t* entry )
 {
     gtk_widget_set_size_request( entry->draw_area, entry->section->graph.w,
